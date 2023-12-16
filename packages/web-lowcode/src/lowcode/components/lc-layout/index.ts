691d422264077c814a1493072f6321f0a4c1b103
maple <2361017046@qq.com>
2023-12-16 18:07:29 +0800

    feat: 添加web-lowcode

---
+++ packages/web-lowcode/src/lowcode/components/lc-layout/index.ts
@@ -0,0 +1,1 @@
+export { default as LcLayout } from './lc-layout.vue';
---
+++ packages/web-lowcode/src/lowcode/components/lc-layout/lc-layout.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+//
+</script>
+<template>
+  <div>lc-layout</div>
+</template>
+<style lang="scss"></style>
